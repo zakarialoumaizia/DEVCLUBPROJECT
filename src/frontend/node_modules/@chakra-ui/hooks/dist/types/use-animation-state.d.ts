a9f1b9f424934f26597d1cd91b05b7db06410ae4
islemchabbi <islemchabbi4@gmail.com>
2025-04-05 20:51:11 +0100

    update

--- src/frontend/node_modules/@chakra-ui/hooks/dist/types/use-animation-state.d.ts
+++
@@ -1,8 +0,0 @@
-export interface UseAnimationStateProps {
-    isOpen: boolean;
-    ref: React.RefObject<HTMLElement | null>;
-}
-export declare function useAnimationState(props: UseAnimationStateProps): {
-    present: boolean;
-    onComplete(): void;
-};
